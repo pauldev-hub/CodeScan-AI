a083bbef4e652d2c38343bc620a7fd3665af2395
pauldev-hub <paulpratyush2@gmail.com>
2026-04-07 14:24:08 +0530

    chore: Initialize database and SQLAlchemy ORM setup

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.models.report import Comment, Report
+from app.models.scan import Finding, Scan
+from app.models.user import User
+
+
+__all__ = ["User", "Scan", "Finding", "Report", "Comment"]
