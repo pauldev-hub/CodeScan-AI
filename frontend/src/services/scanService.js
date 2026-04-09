a731ee86e2d0288b24682ad63c5d8a62440328d7
pauldev-hub <paulpratyush2@gmail.com>
2026-04-09 21:57:55 +0530

    Add frontend: frontend/src/services/scanService.js

---
+++ frontend/src/services/scanService.js
@@ -0,0 +1,61 @@
+import client from "./apiClient";
+import { API_PATHS } from "../utils/constants";
+
+const SCAN_SUBMIT_TIMEOUT_MS = 120000;
+const SCAN_READ_TIMEOUT_MS = 45000;
+
+export const submitScanByUrl = async (githubUrl) => {
+  const response = await client.post(
+    API_PATHS.scanUrl,
+    { github_url: githubUrl },
+    { timeout: SCAN_SUBMIT_TIMEOUT_MS }
+  );
+  return response.data;
+};
+
+export const submitScanByPaste = async (code, language = "text") => {
+  const response = await client.post(
+    API_PATHS.scanPaste,
+    { code, language },
+    { timeout: SCAN_SUBMIT_TIMEOUT_MS }
+  );
+  return response.data;
+};
+
+export const submitScanByUpload = async (files) => {
+  const formData = new FormData();
+  files.forEach((file) => formData.append("file", file));
+
+  const response = await client.post(API_PATHS.scanUpload, formData, {
+    timeout: SCAN_SUBMIT_TIMEOUT_MS,
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  return response.data;
+};
+
+export const getScanStatus = async (scanId, signal) => {
+  const response = await client.get(API_PATHS.scanStatus(scanId), {
+    signal,
+    timeout: SCAN_READ_TIMEOUT_MS,
+  });
+  return response.data;
+};
+
+export const getScanResults = async (scanId, signal) => {
+  const response = await client.get(API_PATHS.scanResults(scanId), {
+    signal,
+    timeout: SCAN_READ_TIMEOUT_MS,
+  });
+  return response.data;
+};
+
+export const getScanHistory = async (page = 1, perPage = 10, signal) => {
+  const response = await client.get(API_PATHS.scanHistory, {
+    params: { page, per_page: perPage },
+    signal,
+    timeout: SCAN_READ_TIMEOUT_MS,
+  });
+  return response.data;
+};
