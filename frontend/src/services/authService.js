e3a8f2ab3fb179eb1bc6cd6dc65e7af36b8bbead
pauldev-hub <paulpratyush2@gmail.com>
2026-04-09 21:57:55 +0530

    Add frontend: frontend/src/services/authService.js

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,19 @@
+import client from "./apiClient";
+import { API_PATHS } from "../utils/constants";
+
+export const login = async ({ email, password }) => {
+  const response = await client.post(API_PATHS.login, { email, password });
+  return response.data;
+};
+
+export const signup = async ({ email, username, password }) => {
+  const response = await client.post(API_PATHS.signup, { email, username, password });
+  return response.data;
+};
+
+export const logout = async (refreshToken) => {
+  if (!refreshToken) {
+    return;
+  }
+  await client.post(API_PATHS.logout, { refresh_token: refreshToken });
+};
