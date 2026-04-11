15f0281964900bfe3d6d286603cd8dc122851c6b
pauldev-hub <paulpratyush2@gmail.com>
2026-04-11 12:59:06 +0530

    commit 41/54: Refactor code block display component with syntax highlighting

--- frontend/src/components/Common/CodeBlock.jsx
+++ frontend/src/components/Common/CodeBlock.jsx
@@ -2,6 +2,9 @@ import Prism from "prismjs";
 import "prismjs/components/prism-python";
 import "prismjs/components/prism-javascript";
 import "prismjs/components/prism-jsx";
+import "prismjs/components/prism-typescript";
+import "prismjs/components/prism-java";
+import "prismjs/components/prism-go";
 import "prismjs/components/prism-json";
 import "prismjs/components/prism-markup";
 
