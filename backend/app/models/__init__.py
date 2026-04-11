8f8432ef3567179a71d109bedb18bbd57b53049a
pauldev-hub <paulpratyush2@gmail.com>
2026-04-11 12:26:43 +0530

    commit 3/54: Update backend models package

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,7 @@
+from app.models.chat import ChatConversation, ChatMessage
 from app.models.report import Comment, Report
 from app.models.scan import Finding, Scan
 from app.models.user import User
 
 
-__all__ = ["User", "Scan", "Finding", "Report", "Comment"]
+__all__ = ["User", "Scan", "Finding", "Report", "Comment", "ChatConversation", "ChatMessage"]
